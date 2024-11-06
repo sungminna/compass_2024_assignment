5b1df4a78455a0d1502823ba069fb0b8c7932eb9
sungminna <sungmin.na330@gmail.com>
2024-11-06 14:26:10 +0900

    feat: add endpoint routes

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// router
+const projectsRouter = require('./routes/projects');
+app.use('/projects', projectsRouter);
+
 module.exports = app;
---
+++ routes/projects.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// import controller 
+const projectController = require('../controllers/projecetController');
+const taskController = require('../controllers/taskController');
+
+
+// project routes
+router.post('/', projectController.createProject);
+router.get('/', projectController.getAllProjects);
+router.get('/:projectId', projectController.getProjectById);
+router.delete('/:projectId', projectController.deleteProject);
+
+// task routes
+router.post('/:projectId/tasks', taskController.createTask);
+router.get('/:projectId/tasks', taskController.getProjectTasks);
+router.put('/:projectId/tasks/:taskId', taskController.updateTask);
+router.delete('/:projectId/tasks/:taskId', taskController.deleteTask);
